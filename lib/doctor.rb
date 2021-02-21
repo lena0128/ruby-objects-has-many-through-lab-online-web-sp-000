b09d1c46858c17d41de356fdb618c4c0ad0e171e
Lena Hou <lenahou2013@gmail.com>
2021-02-21 01:27:21 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def appointments
+end
+
+def new_appointment(date, patient)
+end 
+
+def patients
+end
+
+end
